9f50542b4ea7fcd6d950ede489026aa2f900cedd
nargok <nargok@gmail.com>
2020-01-03 13:41:56 +0900

    cmmand line argument parser

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use clap::{clap_app, crate_version};
+
+
+fn main() {
+    let clap = clap_app!( mdrend =>
+                          (version: crate_version!())
+                          (author: "Taro Yamashita")
+                          (about: "Renders markdown as you like")
+                          (@arg input: +required "Sets the input file")
+    )
+    .get_matches();
+
+    println!("Input = {:?}", clap.value_of("input"));
+
+    println!("done");
+}
